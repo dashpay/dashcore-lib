154523c5107d4cdd0a950c3b17273df89d8d8a83
Kamuela Franco <kamuela.franco@gmail.com>
2018-10-29 14:02:15 +0000

    [EV-1081] Remove payloadSig

--- test/transaction/transaction.js
+++ test/transaction/transaction.js
@@ -1538,7 +1538,6 @@ describe('Transaction', function() {
           proTXHash: '01040eb32f760490054543356cff463865633439dd073cffa570305eb086f70e',
           reason: 0,
           inputsHash: '4f422948637072af5cdc211bb30fe96386c4935f64da82e6a855c6c9f3b37708',
-          payloadSig: '48d6a1bd2cd9eec54eb866fc71209418a950402b5d7e52363bfb75c98e141175',
         };
 
         expect(JSON.stringify(actual)).to.be.deep.equal(JSON.stringify(expected));

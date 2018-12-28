01100aca78a638ff22c2c595b13d4bed43141065
Cofresi <cofresi@dash.org>
2018-12-28 16:40:05 -0400

    prouprevtx: remove unused variable

--- test/transaction/payload/prouprevtxpayload.js
+++ test/transaction/payload/prouprevtxpayload.js
@@ -20,7 +20,6 @@ var validProUpRevTxPayloadJSON = {
 var validProUpRevTxPayloadHexString = '01006f8a813df204873df003d6efc44e1906eaf6180a762513b1c91252826ce05916010082cf248cf6b8ac6a3cdc826edae582ead20421659ed891f9d4953a540616fb4f05279584b3339ed2ba95711ad28b18ee2878c4a904f76ea4d103e1d739f22ff7e3b9b3db7d0c4a7e120abb4952c3574a18de34fa29828f9fe3f52bd0b1fac17acd04f7751967d782045ab655053653438f1dd1e14ba6adeb8351b78c9eb59bf4';
 var validProUpRevTxPayloadBuffer = Buffer.from(validProUpRevTxPayloadHexString, 'hex');
 var validProUpRevTxPayload = ProUpRevTxPayload.fromBuffer(validProUpRevTxPayloadBuffer);
-var validProUpRevTxHash = 'a64e3e06c71873aff149c446d76618efad9a5908007886f6024e9fddb3e6aa13';
 
 describe('ProUpRevTxPayload', function () {
   describe('.fromBuffer', function () {

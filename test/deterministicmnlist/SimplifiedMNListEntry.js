a21c22c1b40cf8456ee7bbfcf285ca9d2407be3e
Anton Suprunchuk <antouhou@gmail.com>
2019-01-09 18:12:33 +0300

    Add changes to SMLEntries from the latest core version

--- test/deterministicmnlist/SimplifiedMNListEntry.js
+++ test/deterministicmnlist/SimplifiedMNListEntry.js
@@ -6,6 +6,7 @@ var expect = require('chai').expect;
 
 var smlEntryWithNoAddress = {
   "proRegTxHash": "01040eb32f760490054543356cff463865633439dd073cffa570305eb086f70e",
+  "confirmedHash": "0000004c938fc648306ae0186125346093c9207b25e69a9b4a057f35afb4d66c",
   "service": "[0:0:0:0:0:0:0:0]:0",
   "keyIDOperator": "0000000000000000000000000000000000000000",
   "keyIDVoting": "c2ae01fb4084cbc3bc31e7f59b36be228a320404",
@@ -15,6 +16,7 @@ var smlEntryWithNoAddressHex = '01040eb32f760490054543356cff463865633439dd073cff
 
 var smlEntryWithSameKeys = {
   "proRegTxHash": "f7737beb39779971e9bc59632243e13fc5fc9ada93b69bf48c2d4c463296cd5a",
+  "confirmedHash": "0000004c938fc648306ae0186125346093c9207b25e69a9b4a057f35afb4d66c",
   "service": "207.154.244.13:19999",
   "keyIDOperator": "43ce12751c4ba45dcdfe2c16cefd61461e17a54d",
   "keyIDVoting": "43ce12751c4ba45dcdfe2c16cefd61461e17a54d",
@@ -23,12 +25,23 @@ var smlEntryWithSameKeys = {
 
 var smlEntryWithDifferentKeys = {
   "proRegTxHash": "75aa128db4cd7679fd88206bd6ef71f57e1b6fe04c2da5515193a6fcd40a47eb",
+  "confirmedHash": "0000004c938fc648306ae0186125346093c9207b25e69a9b4a057f35afb4d66c",
   "service": "159.89.110.184:19999",
   "keyIDOperator": "03d90b1cdc04f1dbe435a4ba51ca2d1ddb53e08c",
   "keyIDVoting": "43ce12751c4ba45dcdfe2c16cefd61461e17a54d",
   "isValid": true
 };
 var smlEntryWithDifferentKeysHex = '75aa128db4cd7679fd88206bd6ef71f57e1b6fe04c2da5515193a6fcd40a47eb0000000000000000000000009f596eb84e1f03d90b1cdc04f1dbe435a4ba51ca2d1ddb53e08c43ce12751c4ba45dcdfe2c16cefd61461e17a54d01';
+
+var smlEntryFromCore = {
+  "proRegTxHash": "32e5ad5cf9a06eb13e0f65cb7ecde1a93ef24995d07355fac2ff05ebd5b9ddbf",
+  "confirmedHash": "0000001960431ec5a566e69f28ae0f6fa3199bd99ec527cccd02f7541d77300c",
+  "service": "95.183.51.146:39999",
+  "pubKeyOperator": "1326ddac1044e0219dba7dccf6b43d1deed3e897717ca06757243b02516cfa67e24026f7a317cf575b40c10e7f6bf7f0",
+  "keyIDVoting": "68976be39d5ef1d43761123f497c96cf4226da87",
+  "isValid": true
+};
+var smlEntryFromCoreHash = "30e8c7bf6dbaabae94e5ef622d7454bf9630aa5f99a0d0611f324df667b74a1f";
 
 describe('SimplifiedMNListEntry', function () {
   describe('constructor', function () {
@@ -89,5 +102,11 @@ describe('SimplifiedMNListEntry', function () {
       expect(entryWithSameKeys.length).to.be.equal(entryWithDifferentKeys.length);
       expect(entryWithNoAddress.length).to.be.equal(entryWithDifferentKeys.length);
     })
+  });
+  describe('calculateHash', function () {
+    it('Should get correct hash', function() {
+      var entry = new SimplifiedMNListEntry(smlEntryFromCore);
+      expect(entry.calculateHash().toString('hex')).to.be.equal(smlEntryFromCoreHash);
+    });
   });
 });

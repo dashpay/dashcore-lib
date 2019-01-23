1633cde02d8590b210abed6824234106975f2940
Anton Suprunchuk <antouhou@gmail.com>
2019-01-23 19:30:20 +0300

    Add some fixes for proregtxpayload

--- test/transaction/payload/proregtxpayload.js
+++ test/transaction/payload/proregtxpayload.js
@@ -9,40 +9,17 @@ var DashcoreLib = require('../../../index');
 var Script = DashcoreLib.Script;
 var ProRegTxPayload = DashcoreLib.Transaction.Payload.ProRegTxPayload;
 
-/*
 var validProRegTxPayloadJSON = {
-  version : 1,
-  protocolVersion : 70211,
-  // 1.2.3.7 mapped to IPv6
-  ipAddress : '00000000000000000000ffff01020307',
-  port : 1237,
-  keyIdOperator : '37ab1f05addb051a4c618a3ccbd1ebd49fe2f60a',
-  keyIdOwner : '37ab1f05addb051a4c618a3ccbd1ebd49fe2f60a',
-  keyIdVoting : '37ab1f05addb051a4c618a3ccbd1ebd49fe2f60a',
-  scriptPayout : '76a9146ba54a9548b7fb962e33114902234eeb93c7fd5c88ac',
-  operatorReward : 1200,
-  collateralIndex: 1,
-  inputsHash : '0da72bc98991885d9e6f617a5b1fca526b03ef12691e9668741f6c8eaad6311e',
-  payloadSig: '1f1260016c46818327d3267ccb2a52e616fa0caef5fc7fce468cd351233b3e364149905ae110e37ea5c06fcf60b8794fc2419004bbca2eba028694a7abe750497c'
-};
-*/
-
-var validProRegTxPayloadJSON = {
-  version : 1,
-  type: 0,
-  mode: 0,
-  collateralHash: '03b10d11e2196319c5632d22dd460b3a31d5fef7c024e2d278e1bbc01661fdef',
-  collateralIndex: 16,
-  ipAddress : '00000000000000000000ffff12ca34aa',
-  port : 35015,
-  keyIdOwner : '9b53742c3abffa67191269b550ab40d619f0259f',
-  keyIdOperator : '15ddc5d0053e2f3a2e70dbb1808ec60844ac4c8ba5fac73c9b6abbab23ef09df0d2347f2a3182eabead0027a2137e59c',
-  keyIdVoting : '9b53742c3abffa67191269b550ab40d619f0259f',
-  operatorReward : 201,
-  scriptPayout : '76a914de77802bd7fcd19277b5229ac44085e3b3a3687088ac',
-  inputsHash : '991c137dc07e13f02e188b2f97725f0f4487a0cd502ac8eb3fbc88e3c2f0d4ca',
-  payloadSigSize : 65,
-  payloadSig: '1fe30ecf9cc167ff85f7d73efe9506de44762447063a62fbe82f19af39d6a353734ad0cd835d6b38cbe5a965b27a2be353ebe4a6a08e50510027c484ba74c4734c'
+    "version": 1,
+    "collateralHash": "a65693797d7137a2529f438807a8c7c085d3984ceaa2f4f5230a36b536cbd326",
+    "collateralIndex": 1,
+    "service": "195.141.0.143:19999",
+    "keyIDOwner": "3c05fb32367a25d8dedc16f741b8492006fb948a",
+    "pubKeyOperator": "8273ec203d1ea62cbdb54e10618329e4ed93e99bc9c5ab2f4cb0055ad281f9ad0808a1dda6aedf12c41c53142828879b",
+    "keyIDVoting": "3c05fb32367a25d8dedc16f741b8492006fb948a",
+    "payoutAddress": "yh9o9kPRK1s3YsuyCBe3DEjBit2RnzhgwH",
+    "operatorReward": 0,
+    "inputsHash": "0b5e6a319019d8f1f4b17da96964df507e417f0a0ef8ca63eaa01e33e05510bc"
 };
 
 var validProRegTxPayloadJSONwitoutCollateral = {
@@ -63,7 +40,7 @@ var validProRegTxPayloadJSONwitoutCollateral = {
   payloadSig: undefined
 };
 
-var validProRegTxPayloadHexString = '010000000000effd6116c0bbe178d2e224c0f7fed5313a0b46dd222d63c5196319e2110db1031000000000000000000000000000ffff12ca34aa88c79f25f019d640ab50b569121967fabf3a2c74539b15ddc5d0053e2f3a2e70dbb1808ec60844ac4c8ba5fac73c9b6abbab23ef09df0d2347f2a3182eabead0027a2137e59c9f25f019d640ab50b569121967fabf3a2c74539bc9001976a914de77802bd7fcd19277b5229ac44085e3b3a3687088ac991c137dc07e13f02e188b2f97725f0f4487a0cd502ac8eb3fbc88e3c2f0d4ca411fe30ecf9cc167ff85f7d73efe9506de44762447063a62fbe82f19af39d6a353734ad0cd835d6b38cbe5a965b27a2be353ebe4a6a08e50510027c484ba74c4734c';
+var validProRegTxPayloadHexString = '01000000000026d3cb36b5360a23f5f4a2ea4c98d385c0c7a80788439f52a237717d799356a60100000000000000000000000000ffffc38d008f4e1f8a94fb062049b841f716dcded8257a3632fb053c8273ec203d1ea62cbdb54e10618329e4ed93e99bc9c5ab2f4cb0055ad281f9ad0808a1dda6aedf12c41c53142828879b8a94fb062049b841f716dcded8257a3632fb053c00001976a914e4876df5735eaa10a761dca8d62a7a275349022188acbc1055e0331ea0ea63caf80e0a7f417e50df6469a97db1f4f1d81990316a5e0b412045323bca7defef188065a6b30fb3057e4978b4f914e4e8cc0324098ae60ff825693095b927cd9707fe10edbf8ef901fcbc63eb9a0e7cd6fed39d50a8cde1cdb4';
 var validProRegTxPayloadHexStringwitoutCollateral = '01000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000ffffc38d8f314e1f140b2fea5ae1aa7e6b8ee68a11d272766339716318ece819b998a36a185e323a8749e55fd3dc2e259b741f8580fbd68cbd9f51d30f4d4da34fd5afc71859dca3cf10fbda595d9f40d00a7ea5cca2502a1c5bc47706688c424c041976a914c0ee80d8e78d59877e5ca6fa6d071f2bbf3037e688ac8e534b752f2a6ab10990caef7fe2ff552bbeadd9e28b99e47e21e69cd22fc78f00';
 
 var validProRegTxPayloadBuffer = Buffer.from(validProRegTxPayloadHexString, 'hex');
@@ -86,24 +63,10 @@ describe('ProRegTxPayload with collateral', function () {
     it('Should return instance of ProRegTxPayload and call #validate on it', function() {
       var payload = ProRegTxPayload.fromBuffer(Buffer.from(validProRegTxPayloadHexString, 'hex'));
 
-      expect(payload.version).to.be.equal(1);
-      expect(payload.type).to.be.equal(0);
-      expect(payload.mode).to.be.equal(0);
-      expect(payload.ipAddress).to.be.equal('00000000000000000000ffff12ca34aa');
-      expect(payload.port).to.be.equal(35015);
-      expect(payload.collateralHash).to.be.equal('03b10d11e2196319c5632d22dd460b3a31d5fef7c024e2d278e1bbc01661fdef');
-      expect(payload.collateralIndex).to.be.equal(16);
-
-      expect(payload.keyIdOwner).to.be.equal('9b53742c3abffa67191269b550ab40d619f0259f');
-      expect(payload.keyIdOperator).to.be.equal('15ddc5d0053e2f3a2e70dbb1808ec60844ac4c8ba5fac73c9b6abbab23ef09df0d2347f2a3182eabead0027a2137e59c');
-      expect(payload.keyIdVoting).to.be.equal('9b53742c3abffa67191269b550ab40d619f0259f');
-
-      expect(new Script(payload.scriptPayout).toAddress('testnet').toString()).to.be.equal('ygbk1qkuT6ReoNMcYUQokjj6aTEyDHpjSQ');
-
-      expect(payload.operatorReward).to.be.equal(201);
-      expect(payload.inputsHash).to.be.equal('991c137dc07e13f02e188b2f97725f0f4487a0cd502ac8eb3fbc88e3c2f0d4ca');
-      expect(payload.payloadSig).to.be.equal('1fe30ecf9cc167ff85f7d73efe9506de44762447063a62fbe82f19af39d6a353734ad0cd835d6b38cbe5a965b27a2be353ebe4a6a08e50510027c484ba74c4734c');
-      // TODO: Add signature verification
+      expect(payload.toJSON({
+          network: 'testnet',
+          skipSignature: true
+      })).to.be.deep.equal(validProRegTxPayloadJSON);
     });
 
     it('Should throw in case if there is some unexpected information in raw payload', function() {
@@ -123,24 +86,8 @@ describe('ProRegTxPayload with collateral', function () {
 
     it('Should return instance of ProRegTxPayload and call #validate on it', function() {
       var payload = ProRegTxPayload.fromJSON(validProRegTxPayloadJSON);
-      expect(payload.version).to.be.equal(1);
-      expect(payload.type).to.be.equal(0);
-      expect(payload.mode).to.be.equal(0);
-      expect(payload.collateralHash).to.be.equal('03b10d11e2196319c5632d22dd460b3a31d5fef7c024e2d278e1bbc01661fdef');
-      expect(payload.collateralIndex).to.be.equal(16);
-      expect(payload.ipAddress).to.be.equal('00000000000000000000ffff12ca34aa');
-      expect(payload.port).to.be.equal(35015);
 
-      expect(payload.keyIdOwner).to.be.equal('9b53742c3abffa67191269b550ab40d619f0259f');
-      expect(payload.keyIdOperator).to.be.equal('15ddc5d0053e2f3a2e70dbb1808ec60844ac4c8ba5fac73c9b6abbab23ef09df0d2347f2a3182eabead0027a2137e59c');
-      expect(payload.keyIdVoting).to.be.equal('9b53742c3abffa67191269b550ab40d619f0259f');
-
-      expect(new Script(payload.scriptPayout).toAddress('testnet').toString()).to.be.equal('ygbk1qkuT6ReoNMcYUQokjj6aTEyDHpjSQ');
-
-      expect(payload.operatorReward).to.be.equal(201);
-      expect(payload.inputsHash).to.be.equal('991c137dc07e13f02e188b2f97725f0f4487a0cd502ac8eb3fbc88e3c2f0d4ca');
-      expect(payload.payloadSig).to.be.equal('1fe30ecf9cc167ff85f7d73efe9506de44762447063a62fbe82f19af39d6a353734ad0cd835d6b38cbe5a965b27a2be353ebe4a6a08e50510027c484ba74c4734c');
-      // TODO: Add signature verification
+      expect(payload.toJSON()).to.be.deep.equal(validProRegTxPayloadJSON);
     });
 
     after(function () {

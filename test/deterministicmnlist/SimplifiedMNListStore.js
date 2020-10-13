52e6313481c1667c198c3b282f4f57333272e38d
Cofresi <cofresi@dash.org>
2020-10-13 11:23:09 -0400

    add test file

---
+++ test/deterministicmnlist/SimplifiedMNListStore.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;
+var QuorumEntry = require('../../lib/deterministicmnlist/QuorumEntry');
+var SimplifiedMNListStore = require('../../lib/deterministicmnlist/SimplifiedMNListStore');
+var SimplifiedMNList = require('../../lib/deterministicmnlist/SimplifiedMNList');
+var SMNListFixture = require('../fixtures/mnList');
+
+describe('SimplifiedMNListStore', function () {
+  describe('quorum signatures', function () {
+/*
+    it('Should get a SimplifiedMNList by block height', function () {
+      // const mnList = new SimplifiedMNList(SMNListFixture.getFirstDiff());
+      const height = 1111;
+      const SMLStore = new SimplifiedMNListStore(SMNListFixture.getFirstDiff());
+      const currentSML = SMLStore.getSMLbyHeight(height);
+      expect(currentSML.blockHash).to.equal(SMNListFixture.getFirstDiff().blockHash);
+    });
+*/
+  });
+});

7c3f61cc07a569786bc6a46c22921eba901fda04
Anton Suprunchuk <antouhou@gmail.com>
2020-12-24 12:59:19 +0300

    add jsdoc to the ChainLockSigMessage constructor

--- typings/zmqMessages/ChainLockSigMessage.d.ts
+++ typings/zmqMessages/ChainLockSigMessage.d.ts
@@ -2,6 +2,10 @@ import { Block } from "../block/Block";
 import { ChainLock } from "../chainlock/ChainLock";
 
 export class ChainLockSigMessage {
+  /**
+   * Parses raw buffer from rawchainlocksig zmq message
+   * @param {Buffer} chainLockSigMessageBuffer - contents of awchainlocksig zmq message
+   */
   constructor(chainLockSigMessageBuffer: Buffer);
 
   readonly block: Block;

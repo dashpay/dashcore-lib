327117cdbc94898509cf988a78cbd47c5099a8a6
markin.io <markin.io210@gmail.com>
2023-06-16 14:09:09 +0100

    feat: asset lock tx payload

---
+++ typings/transaction/payload/Payload.d.ts
@@ -0,0 +1,27 @@
+import { ProRegTxPayload } from './ProRegTxPayload';
+import { ProUpRegTxPayload } from './ProUpRegTxPayload';
+import { ProUpRevTxPayload } from './ProUpRevTxPayload';
+import { ProUpServTxPayload } from './ProUpServTxPayload';
+import { SubTxCloseAccountPayload } from './SubTxCloseAccountPayload';
+import { SubTxRegisterPayload } from './SubTxRegisterPayload';
+import { SubTxResetKeyPayload } from './SubTxResetKeyPayload';
+import { SubTxTopupPayload } from './SubTxTopupPayload';
+import { SubTxTransitionPayload } from './SubTxTransitionPayload';
+import { CoinbasePayload } from './CoinbasePayload';
+import { CommitmentTxPayload } from './CommitmentTxPayload';
+import { AssetLockPayload } from './AssetLockPayload';
+
+export namespace Payload {
+  export { ProRegTxPayload };
+  export { ProUpRegTxPayload };
+  export { ProUpRevTxPayload };
+  export { ProUpServTxPayload };
+  export { SubTxCloseAccountPayload };
+  export { SubTxRegisterPayload };
+  export { SubTxResetKeyPayload };
+  export { SubTxTopupPayload };
+  export { SubTxTransitionPayload };
+  export { CoinbasePayload };
+  export { CommitmentTxPayload };
+  export { AssetLockPayload };
+}
